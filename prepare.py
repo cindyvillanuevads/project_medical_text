12a1650fa6f3d09f75a33e1b6117852b26b7d86a
Cindy Villanueva <cindy.villanueva.ds@gmail.com>
2021-09-21 13:45:02 -0500

    add scripts for def basic_clean (string):

--- prepare.py
+++ prepare.py
@@ -69,3 +69,117 @@ def top_n_target(df,target,  n):
     #get new df with only the top n values of target
     df= df[df.medical_specialty.isin(topl)].reset_index(drop=True)
     return  df
+
+
+def basic_clean (string):
+    '''
+    takes in a string and lowercase everything, normalize unicode characters, replace anything that is not a letter,
+    number, whitespace or a single quote.
+    retunr a clean string
+    '''
+    
+    string = string.lower()
+    string = unicodedata.normalize('NFKC',string)\
+    .encode('ascii', 'ignore')\
+    .decode('utf-8')
+    string = re.sub(r"\w*.?\w*@\w*.com", '', string)
+    string = re.sub(r"[^a-z0-9\s]", '', string)
+    string = re.sub(r'\w*http\w*', '', string)
+    string = re.sub(r'\w*github\w*', '', string)
+    string = re.sub(r'\w*html\w*', '', string)
+    string = re.sub(r'\w*gmail\w*', '', string)
+    string = re.sub(r'\w*\n\w*', '', string)
+
+    return string
+
+
+def tokenize (string):
+    '''
+    take in a string and tokenize all the words in the string
+    '''
+    
+    # Create the tokenizer
+    tokenizer = nltk.tokenize.ToktokTokenizer()
+    # Use the tokenizer
+    string = tokenizer.tokenize(string, return_str = True)
+    return string
+
+
+def stem (string):
+    '''
+     takes in a text and return the text after applying stemming to all the words.
+    '''
+    # Create porter stemmer.
+    ps = nltk.porter.PorterStemmer()
+    # Apply the stemmer to each word in our string.
+    stems = [ps.stem(word) for word in string.split()]
+    text_stemmed = ' '.join(stems)
+    return text_stemmed 
+
+
+
+def lemmatize (text):
+    '''
+     Takes in some text and return the text after applying lemmatization to each word.
+    '''
+    wnl = nltk.stem.WordNetLemmatizer()
+    
+    # Use the lemmatizer on each word in the list of words we created by using split.
+    lemmas = [wnl.lemmatize(word) for word in text.split()]
+    
+    # Join our list of words into a string again; assign to a variable to save changes.
+    text_lemmatized = ' '.join(lemmas)
+    
+    return text_lemmatized
+    
+
+
+def remove_stopwords (string, extra_words= [], exclude_words=[]):
+    '''
+    Takes in a strand return the text after removing all the stopwords.
+    Parameters:
+    string : text in  string type
+    extra_words : list of additional stop words to include,
+    exclude_words : list of any words that we don't want to remove.
+    '''
+    # standard English language stopwords list from nltk
+    stopword_list = stopwords.words('english')
+    
+    #add extra_words
+    stopword_list =stopword_list + extra_words
+    
+    #exclude words
+    for element in exclude_words:
+        if element in stopword_list:
+            stopword_list.remove(element)
+    # Split words.
+    words = string.split()
+    
+    # Create a list of words from my string with stopwords removed and assign to variable.
+    filtered_words = [word for word in words if word not in stopword_list]
+    
+    # Join words in the list back into strings; assign to a variable to keep changes.
+    string_without_stopwords = ' '.join(filtered_words)
+    
+    return string_without_stopwords
+
+
+
+def prepare_data(df, column, extra_words=[], exclude_words=[]):
+    '''
+    This function take in a df and the string name for a text column with 
+    option to pass lists for extra_words and exclude_words and
+    returns a df with the  original text, cleaned (tokenized and stopwords removed) , stemmed text,
+    lemmatized text.
+    '''
+    df['clean'] = df[column].apply(basic_clean)\
+                            .apply(tokenize)\
+                            .apply(remove_stopwords, 
+                                   extra_words=extra_words, 
+                                   exclude_words=exclude_words)
+    
+    df['stemmed'] = df['clean'].apply(stem)
+    
+    df['lemmatized'] = df['clean'].apply(lemmatize)
+    
+    return df
